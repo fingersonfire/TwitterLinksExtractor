68136e08ed867a9366718674147b85985716b345
fingersonfire <fingersonfirecom@gmail.com>
2020-03-24 10:47:49 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# TwitterLinksExtractor
+Retreive Tweets from a User Timeline and export all links to separated text files
+
+## Requirements
+- Bash
+- Python3
+- TWINT (https://github.com/twintproject/twint)
+- CSV Tools
+
+## Tested on
+- Ubuntu 18.04
+
+## Installation (Ubuntu 18.04 LTS)
+Install python3 and pip3 : 
+sudo apt install python3 python3-pip
+
+Install CSVTool : 
+sudo apt install csvtool
+
+Clone this repository in your favorite location
+
+## Usage
+
+### Run the tool
+Run the bash script from this repository with the Twitter username as a parameter, without the @ sign.
+eg: ./tle.sh jack
+
+### What the script is actually doing
+The script will be creating repository for all the extract your will run in a folder structure like this one:
+/home/(YourUsername)/TwitterLinkExtractor/(TwitterUsername)/(DateTime)/
+
+Every stage of the extraction creates a different file :
+- A CSV file with all the tweets from the user's TL
+- A text file with only the tweets without all other info (extract of column 11 from the CSV using CSVTool)
+- A text file of all the links the script found
+- Separate files for each type of links
+
+## Supported links
+At the moment the script separates links from the following sources:
+- Youtube
+- Spotify
+- Deezer
+
+It could simply adapted to catch other webistes.
---
+++ tle.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Extract Tweets for a user using TWINT then extract Youtube links from it
+#
+# CSV separator is COMMA
+#
+# Requirements : 
+#  - TWINT
+#  - CSV tool (sudo apt install csvtool)
+
+# You can modify this link to the location you prefer
+BASEDIR=~/TwitterLinksExtractor
+
+# Do not modify these values
+USERNAME=$1
+DATETIME=$(date +'%Y%m%d-%H%M%S')
+WORKDIR="$BASEDIR/$USERNAME/$DATETIME"
+TWEETLISTCSV="$WORKDIR/tweetlist.csv"
+TWEETLIST="$WORKDIR/tweetlist.txt"
+URLSLIST="$WORKDIR/urlslist.txt"
+URLSYOUTUBE="$WORKDIR/urlsyoutube.txt"
+URLSSPOTIFY="$WORKDIR/urlsspotify.txt"
+URLSDEEZER="$WORKDIR/urlsdeezer.txt"
+
+# Initial Display
+echo "Username: $USERNAME"
+echo "Working Directory : $WORKDIR"
+
+# Directory creation
+echo
+echo "Directory Creation"
+
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+
+# Extract tweets to CSV using Twint
+echo
+echo "Extraction of the tweets"
+
+twint -u $USERNAME -o $TWEETLISTCSV --csv
+
+
+# Extract Tweets column only from CSV
+echo
+echo "Extraction of the column containing only tweets"
+
+# AWK command does not work if the column has carriage returns in cells
+#awk -F "\"*,\"*" '{print $11}' $TWEETLISTCSV > $TWEETLIST
+
+# Using CSV Tool instead
+csvtool col 11 $TWEETLISTCSV > $TWEETLIST
+
+# Extract URLS
+echo
+echo "Extraction of the URLS"
+
+cat $TWEETLIST | grep http | grep -shoP 'http.*?[" >]' > $URLSLIST
+
+
+# Normalizing URLs (Remove trailing dots, spaces, Remove mobile, use long Youtube addresses)
+echo
+echo "Normalisation of the URLS"
+
+sed -e "s/ …//g" -i $URLSLIST
+sed -e "s/ //g" -i $URLSLIST
+sed 's,",,g' -i $URLSLIST
+sed 's,//m.,//,g' -i $URLSLIST
+sed 's,youtu.be/,youtube.com/watch?v=,g' -i $URLSLIST
+
+# Select only Youtube.com links
+echo
+echo "Extraction of the Youtube URLS"
+cat $URLSLIST | grep youtu > $URLSYOUTUBE
+
+echo
+echo "Extraction of the Youtube URLS"
+cat $URLSLIST | grep spotify > $URLSSPOTIFY
+
+echo
+echo "Extraction of the Youtube URLS"
+cat $URLSLIST | grep deezer > $URLSDEEZER
+
